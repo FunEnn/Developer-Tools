9ab60142d59eaf1321039647f33e999b766461aa
enn <3095852337@qq.com>
2024-12-13 21:09:28 +0800

    new

--- vite.config.ts
+++ vite.config.ts
@@ -33,5 +33,16 @@ export default defineConfig({
       },
     },
   },
-  base: "/Developer-Tools/",
+  base: "/",
+  build: {
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
+  },
 });
