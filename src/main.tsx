80a91c6bd431fabe368ca70a47d42bbebd8229b6
enn <3095852337@qq.com>
2024-12-13 19:48:37 +0800

    new

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { router } from "./router";
 import "./index.css";
 
@@ -10,5 +11,6 @@ if (!rootElement) throw new Error('Failed to find the root element');
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <SpeedInsights />
   </React.StrictMode>
 )
