bec7e973b591fc596b9ae23d097cf2c09b3c373b
FunEnn <3095852337@qq.com>
2025-07-26 19:53:40 +0800

    refactor: md编辑器预览区域样式修改

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -7,7 +7,7 @@ export { default as ImageConverter } from "./image-tools/ImageConverter";
 export { default as ImageCompressor } from "./image-tools/ImageCompressor";
 export { default as SvgEditor } from "./image-tools/SvgEditor";
 
-export { QrCodeTool } from "./dev-tools/QrCodeTool";
+export { default as QrCodeTool } from "./dev-tools/QrCodeTool";
 export { default as DataVisualization } from "./dev-tools/DataVisualization";
 export { default as JsonEditor } from "./dev-tools/JsonEditor";
 export { default as MarkdownEditor } from "./dev-tools/MarkdownEditor";
